21e91780c9fd5f6591edb27879e0c6443d145e97
Michal Mancewicz <nelsonoftrafalgar@gmail.com>
2021-04-05 22:28:22 +0200

    fix negative seconds

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 
 import { ISequence } from './Setup'
 
@@ -7,40 +7,40 @@ interface IProps {
 }
 
 const Timer: FC<IProps> = ({ sequence }) => {
-	const [step, setStep] = useState(0)
+	const step = useRef(0)
 	const [seconds, setSeconds] = useState(+sequence[0].interval)
 	const [active, setActive] = useState(false)
 
 	useEffect(() => {
-		if (step < sequence.length) setSeconds(+sequence[step].interval)
-	}, [step, sequence])
-
-	useEffect(() => {
 		let timer: number = 0
 
-		if (active && seconds < 0) {
-			setStep((step) => step + 1)
-			setActive(false)
+		if (active && seconds === 0) {
+			timer = window.setInterval(() => {
+				step.current++
+				if (step.current < sequence.length) setSeconds(+sequence[step.current].interval)
+				setActive(false)
+			}, 1000)
 		}
 
-		if (active) timer = window.setInterval(() => setSeconds((seconds) => seconds - 1), 1000)
-		else clearInterval(timer)
+		if (active && seconds > 0) {
+			timer = window.setInterval(() => setSeconds((seconds) => seconds - 1), 1000)
+		}
 
 		return () => {
 			clearInterval(timer)
 		}
-	}, [active, seconds])
+	}, [active, seconds, sequence])
 
 	return (
 		<>
-			{step === sequence.length ? (
+			{step.current === sequence.length ? (
 				<div className='d-flex align-items-center justify-content-center h-100'>
 					<span className='done'>Done!</span>
 				</div>
 			) : (
 				<div className='d-flex flex-column align-items-center justify-content-between h-100'>
-					<span className='timer-to-go'>To go: {sequence.length - step}</span>
-					<span className='timer-sequence px-5'>{sequence[step].count}</span>
+					<span className='timer-to-go'>To go: {sequence.length - step.current}</span>
+					<span className='timer-sequence px-5'>{sequence[step.current].count}</span>
 					<span className='timer-counter'>{seconds} s</span>
 					<button className='button' onClick={() => setActive(!active)}>
 						{active ? 'Pause' : 'Start'}
